01bb9ccbf191e98bc572d94db2676cabc3cfb647
Hesam <jupihes@users.noreply.github.com>
2024-11-02 11:41:21 +0330

    remove `inplace=True`

--- file size extraction.py
+++ file size extraction.py
@@ -79,7 +79,7 @@ writer = pd.ExcelWriter(r'D:\Big file list.xlsx', engine= 'xlsxwriter')
 
 for i in folders:
     name = 'Files at ' + i.replace('\\','').replace(':','')
-    df = folder_files(folder = i, size = 200).sort_values('Size_MB', ascending=False,inplace=True)
+    df = folder_files(folder = i, size = 200).sort_values('Size_MB', ascending=False)
     df.to_excel(writer, name, index = False)
 
 writer.save()
